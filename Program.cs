7ea007e46c5e43ce3689dd3396980b0c599b5cca
OutsiderFeng <704483396@qq.com>
2020-07-19 23:16:40 +0800

    New text

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace property_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student() { Age = 16};
+            Console.WriteLine(stu.CanWork);
+        }
+    }
+    class Student
+    {
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value;
+                this.caculatorCanWork();
+            }
+        }
+
+        private bool canWork;
+
+        public bool CanWork
+        {
+            get { return canWork; }
+        }
+
+        private void caculatorCanWork()
+        {
+            if (this.age>=16)
+            {
+                this.canWork = true;
+            }
+            else
+            {
+                this.canWork = false;
+            }
+        }
+    }
+}
